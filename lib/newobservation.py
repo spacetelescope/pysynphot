1386a622c8cc6f8a255f14912b7c76b6d34a23ef
laidler <laidler@stsci.edu>
2007-05-15 15:27:16 +0000

    Continued implementation of Ticket #33: refactor Observation class.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@211 90a0a646-be8a-0410-bb88-9290da87bc01

--- lib/newobservation.py
+++ lib/newobservation.py
@@ -5,6 +5,7 @@ certain other methods."""
 
 import spectrum
 import units
+import numpy as N
 
 class Observation(spectrum.CompositeSourceSpectrum):
     """An Observation is the end point of a chain of spectral manipulation.
@@ -19,35 +20,63 @@ class Observation(spectrum.CompositeSourceSpectrum):
         self.spectrum = self.component1
         self.bandpass = self.component2
 
+        #The natural waveset of the observation is the merge of the
+        #natural waveset of the spectrum with the natural waveset of the
+        #bandpass. Because the Observation inherits from a
+        #CompositeSourceSpectrum, this will be handled correctly.
 
-    def getBinnedArrays(self,binset=None):
-        binwave = self.GetBinset(binset)
-        binflux = self(binwave)
+        self.initbinset(binset)
+        self.initbinflux()
 
-        binflux = units.Photlam().Convert(binwave,binflux,self.fluxunits.name)
-        binwave = units.Angstrom().Convert(binwave, self.waveunits.name)
-
-        return (binwave, binflux)
-
-    def _getBinsetProp(self):
-        binwave, binflux = self.getBinnedArrays()
-        return binwave
-
-    def _getBinfluxProp(self):
-        binwave, binflux = self.getBinnedArrays()
-        return binflux
-    
-    binwave = property(_getBinsetProp,doc='Binned wavelength property')
-    binflux = property(_getBinfluxProp,doc='Flux on binned wavelength set property')
-    def GetBinset(self,binset=None):
-        if binset is None:
+    def initbinset(self,binset=None):
+        if binset == None:
             try:
-                binset = self.bandpass.obsmode.bandWave()
+                self.binwave = self.bandpass.obsmode.bandWave()
             except (KeyError, AttributeError), e:
                 print "%s does not have a defined binset in the wavecat table. You must provide a binset."%self.bandpass
                 raise e
-        return binset
+        else:
+            self.binwave=binset
 
+
+    def initbinflux(self):
+        """This routine performs the integration of the spectrum
+        on the specified binned waveset. It uses the natural waveset
+        of the spectrum in performing this integration."""
+
+        # compute endpoints of each summation bin.
+        midpoints = (self.binwave[1:] + self.binwave[:-1]) / 2.0
+        endpoints = N.empty(shape=[len(midpoints)+2,],dtype=N.float64)
+        endpoints[1:-1] = midpoints
+        endpoints[0]  = self.binwave[0] - (midpoints[0] - self.binwave[0])
+        endpoints[-1] = self.binwave[-1] + (self.binwave[-1] - midpoints[-1])
+        
+        # compute indices associated to each endpoint.
+        indices = N.searchsorted(self.wave, endpoints)
+        diff = indices[1:] - indices[:-1]
+        self._indices = indices[:-1]
+        self._indices_last = self._indices + diff 
+
+        # prepare integration variables.
+        self._deltaw = self.binwave[1:] - self.binwave[:-1]
+        flux = self.spectrum(self.binwave)
+        avflux = (flux[1:] + flux[:-1]) / 2.0
+        
+        # sum over each bin.
+        self._binflux = N.empty(shape=self.binwave.shape,dtype=N.float64)
+        for i in range(len(self._indices)):
+            first = self._indices[i]
+            last = self._indices_last[i]
+            self._binflux[i]=(avflux[first:last]*self._deltaw[first:last]).sum() 
+
+    def _getBinfluxProp(self):
+        binflux = units.Photlam().Convert(self.binwave,
+                                          self._binflux,
+                                          self.fluxunits.name)
+        return binflux
+    
+    binflux = property(_getBinfluxProp,doc='Flux on binned wavelength set property')
+ 
     #Disable methods that should not be supported by this class
     def __mul__(self, other):
         raise NotImplementedError('Observations cannot be multiplied')
