5af6e50d2c610cde7f6c02d71320df1548ff8299
laidler <laidler@stsci.edu>
2007-10-26 15:41:43 +0000

    Ticket #45: save the subversion revision number to a specified file as part of the revision process. Also incorporated this revision number into the version string passed to distutils.setup, and added some comments.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@272 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -2,18 +2,26 @@ from distutils.core import setup, Extension
 import sys, os.path, string, shutil, glob, os, re
 from distutils.command.install_data import install_data
 import glob
+import commands
 
-
+#Determine the python version
 ver = sys.version_info
 python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
 
+#Determine, and save, the revision number
+revset=commands.getoutput('svnversion .')
+vfname=os.path.join('data','generic','versioninfo.dat')
+f=open(vfname,'w')
+f.write(revset)
+f.close()
+if (revset.strip() == 'exported'):
+    print "WARNING, no SVN version info available for this package"
+    print "Proceeding with installation.\n"
+    
+#Define the datafiles that are part of this package
 DATA_FILES = glob.glob(os.path.join('data', 'generic', '*'))
 WAVECAT_FILES = glob.glob(os.path.join('data', 'wavecat', '*'))
-
-
 DATA_FILES_DIR = os.path.join('pysynphot', 'data')
-
-
 PYSYNPHOT_DATA_FILES = [(DATA_FILES_DIR,DATA_FILES), (DATA_FILES_DIR, WAVECAT_FILES)]
 
 
@@ -47,7 +55,7 @@ tpmsrc.extend(['src/blackbox.c','src/pytpm_wrap.c'])
 def dosetup():
     print sys.prefix
     r = setup(name = "pysynphot",
-              version = "0.21dev",
+              version = "0.3dev%s"%revset.strip(),
               description  = 'Python Synthetic Photometry Utilities',
               fullname     = 'AstroLib Pysynphot',
               license      = 'BSD',
