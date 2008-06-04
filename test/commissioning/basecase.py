c300d1f21afc5142496392e6ba107795f4f06beb
laidler <laidler@stsci.edu>
2008-06-04 23:03:06 +0000

    Implement additional tests; apply some refactoring
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@445 90a0a646-be8a-0410-bb88-9290da87bc01

--- test/commissioning/basecase.py
+++ test/commissioning/basecase.py
@@ -5,6 +5,7 @@ from pysynphot import newetc as etc
 from pyraf import iraf
 from iraf import stsdas,hst_calib,synphot
 import os,time
+from pysynphot.wavetable import wavetable as Wavecat
 
 
 class calcspecCase(testutil.LogTestCase):
@@ -27,6 +28,28 @@ class calcspecCase(testutil.LogTestCase):
                  'Thresh':self.thresh}
         self.tra={}
 
+    def run_calcspec(self,obsmode,spstring,form,output=None,binset=False):
+        if binset:
+            wavetab=Wavecat[self.obsmode]
+        else:
+            wavetab=""
+
+        if obsmode in (None,"None"):
+            spec=spstring
+        else:
+            spec="band(%s)*(%s)"%(obsmode,spstring)
+
+        iraf.calcspec(spectrum=spec,
+                      output=output,
+                      form=form,wavetab=wavetab)
+
+    def run_countrate(self,form,output=None):
+        if output is None:
+            output=""
+        iraf.countrate(spectrum=self.spectrum,magnitude="",
+                       instrument=self.obsmode,
+                       output=output,form=form)
+
     def runpy(self):
         self.sptest=etc.parse_spec(self.spectrum)
         self.csname=self.name+'.fits'
@@ -41,34 +64,21 @@ class calcspecCase(testutil.LogTestCase):
         ans=(test[idx]-ref[idx])/ref[idx]
         return ans
 
-
-    def testphotlam(self):
-        iraf.calcspec(spectrum=self.spectrum,form='photlam',
-                      output=self.csname)
-        spref=S.FileSpectrum(self.csname)
-        rflux=spref.flux
-        tflux=self.sptest(spref.wave)
-        
-        self.adiscrep=self.arraydiff(tflux,rflux)
+    def arraytest(self,test,ref):
+        self.adiscrep=self.arraydiff(test,ref)
         self.tra['Discrepmin']=self.adiscrep.min()
         self.tra['Discrepmax']=self.adiscrep.max()
         self.failUnless(N.alltrue(abs(self.adiscrep)<self.thresh),msg="Worst case %f"%abs(self.adiscrep).max())
 
 
-##     def log(self,status):
-##         f=open(self.name+'.log','w')
-##         f.write("%s:: Status=%s\n"%(self.name,status))
-##         f.write("%s:: Time=%s\n"%(self.name,time.asctime()))
-##         f.write("%s:: ta_Obsmode=%s\n"%(self.name,self.obsmode))
-##         f.write("%s:: ta_Spectrum=%s\n"%(self.name,self.spectrum))
-##         try:
-##             f.write("%s:: ra_Discrepmax=%g\n"%(self.name,self.discrep[1]))
-##             f.write("%s:: ra_Discrepmin=%g\n"%(self.name,self.discrep[0]))
-##         except TypeError:
-##             f.write("%s:: ra_Discrep=%g\n"%(self.name,self.discrep))
-##         if status != 'P':
-##             f.write("%s:: ra_Trace=%s\n"%(self.name,str(self._exc_info())))
-##         f.close()
+    def testspecphotlam(self):
+        self.run_calcspec(None,self.spectrum,'photlam',self.csname)
+        spref=S.FileSpectrum(self.csname)
+        self.sptest.convert('photlam')
+        rflux=spref.flux
+        tflux=self.sptest(spref.wave)
+        
+        self.arraytest(tflux,rflux)
         
 class calcphotCase(calcspecCase):
         
@@ -76,14 +86,16 @@ class calcphotCase(calcspecCase):
         self.sptest=etc.parse_spec(self.spectrum)
         self.bp=S.ObsBandpass(self.obsmode)
         self.cbname=self.name+'.fits'
-        try:
-            os.remove(self.cbname)
-        except OSError:
-            pass
+        self.csname=self.name+'_cs.fits'
+        for fname in (self.cbname, self.csname):
+            try:
+                os.remove(fname)
+            except OSError:
+                pass
         self.discrep=-99
         
-    def testphotlam(self):
-        pass
+
+     
                     
     def testthru(self):
         iraf.calcband(obsmode=self.obsmode,output=self.cbname)
@@ -92,10 +104,7 @@ class calcphotCase(calcspecCase):
         rwave=ref.wave
         tthru=self.bp(rwave)
         
-        self.adiscrep=self.arraydiff(tthru,rthru)
-        self.tra['Discrepmin']=self.adiscrep.min()
-        self.tra['Discrepmax']=self.adiscrep.max()
-        self.failUnless(N.alltrue(abs(self.adiscrep)<self.thresh),msg="Worst case %f"%abs(self.adiscrep).max())
+        self.arraytest(tthru,rthru)
 
         
     def testefflam(self):
@@ -110,12 +119,66 @@ class calcphotCase(calcspecCase):
 
         
 
-class countrateCase(calcspecCase):
-    def tearDown(self):
-        pass
+class countrateCase(calcphotCase):
+    def runpy(self):
+        self.sptest=etc.parse_spec(self.spectrum)
+        self.bp=S.ObsBandpass(self.obsmode)
+        self.csname=self.name+'_cs.fits'
+        self.crname=self.name+'_cr.fits'
+        self.cbname=self.name+'.fits'
+        for fname in (self.cbname, self.csname, self.crname):
+            try:
+                os.remove(fname)
+            except OSError:
+                pass
 
+    def testcsphotlam(self):
+        obs=S.Observation(self.sptest,self.bp)
+        obs.convert('photlam')
+        self.run_calcspec(self.obsmode,self.spectrum,
+                          'photlam',self.csname,binset=True)
+        spref=S.FileSpectrum(self.csname)
+        rflux=spref.flux
+        tflux=obs.binflux
+        self.arraytest(tflux,rflux)
 
+    def testcscounts(self):
+        obs=S.Observation(self.sptest,self.bp)
+        obs.convert('counts')
+        self.run_calcspec(self.obsmode,self.spectrum,
+                          'counts',self.csname.replace('.fits','_counts.fits'),
+                          binset=True)
+        spref=S.FileSpectrum(self.csname.replace('.fits','_counts.fits'))
+        rflux=spref.flux
+        tflux=obs.binflux
+        self.arraytest(tflux,rflux)
+ 
+    def testcrphotlam(self):
+        obs=S.Observation(self.sptest,self.bp)
+        obs.convert('photlam')
+        self.run_countrate('photlam',self.crname.replace('.fits','_cr.fits'))
+        spref=S.FileSpectrum(self.crname.replace('.fits','_cr.fits'))
+        rflux=spref.flux
+        tflux=obs.binflux
+        self.arraytest(tflux,rflux)
 
+    def testcrcounts(self):
+        obs=S.Observation(self.sptest,self.bp)
+        obs.convert('counts')
+        self.run_countrate('counts',self.crname.replace('.fits','_counts.fits'))
+        spref=S.FileSpectrum(self.crname.replace('.fits','_counts.fits'))
+        rflux=spref.flux
+        tflux=obs.binflux
+        self.arraytest(tflux,rflux)
 
+    def testcountrate(self):
+        obs=S.Observation(self.sptest,self.bp)
+        self.run_countrate('counts')
+        rval=iraf.countrate.getParam('flux_tot',native=1)
+        tval=obs.countrate()
+        self.discrep=(tval-rval)/rval
+        self.tra['Discrep']=self.discrep
+        self.failUnless(abs(self.discrep) < self.thresh,msg="Discrep=%f"%self.discrep)
+      
 class SpecSourcerateSpecCase:
     pass
