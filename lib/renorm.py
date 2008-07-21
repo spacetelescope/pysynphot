7cc544ae18b6e7021f66bb7b9521db2a79c75c04
laidler <laidler@stsci.edu>
2008-07-21 20:49:44 +0000

    Forgot renorm.py
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@537 90a0a646-be8a-0410-bb88-9290da87bc01

---
+++ lib/renorm.py
@@ -0,0 +1,68 @@
+"""Segregate all the renormalization functions here. Once we have
+one that works, turn it into a method on the spectrum class."""
+import math
+import numpy as N
+from spectrum import UnitSpectrum, Vega, default_waveset
+import units
+
+
+def DefineStdSpectraForUnits():
+    """Adorn the units with the appropriate kind of spectrum for
+    renormalizing. This is done here to avoid circular imports."""
+    
+    #Linear flux-density units
+    units.Flam.StdSpectrum = UnitSpectrum(1,fluxunits='flam')
+    units.Fnu.StdSpectrum = UnitSpectrum(1,fluxunits='fnu')
+    units.Photlam.StdSpectrum = UnitSpectrum(1,fluxunits='photlam')
+    units.Photnu.StdSpectrum = UnitSpectrum(1,fluxunits='photnu')
+    units.Jy.StdSpectrum = UnitSpectrum(1,fluxunits='jy')
+    units.mJy.StdSpectrum = UnitSpectrum(1,fluxunits='mjy')
+
+    #Non-density units
+    scale = 1.0/default_waveset.size
+    units.Counts.StdSpectrum = UnitSpectrum(1,fluxunits='counts')*scale
+    units.OBMag.StdSpectrum = UnitSpectrum(1,fluxunits='counts')*scale
+
+    #Magnitude flux-density units
+    units.ABMag.StdSpectrum = UnitSpectrum(3.63e-20,fluxunits='fnu')
+    units.STMag.StdSpectrum = UnitSpectrum(3.63e-9, fluxunits='flam')
+    units.VegaMag.StdSpectrum = Vega
+
+
+#Call this function so the attributes get added upon import.
+DefineStdSpectraForUnits()
+
+
+def StdRenorm(spectrum, band, RNval, RNunitstring):
+    """Another approach to renormalization"""
+
+    #Compute the flux of the spectrum through the bandpass and make sure
+    #the result makes sense.
+    sp = spectrum*band
+    totalflux = sp.integrate()
+    if totalflux <= 0.0:
+        raise ValueError('Integrated flux is <= 0')
+    if N.isnan(totalflux):
+        raise ValueError('Integrated flux is NaN')
+    if N.isinf(totalflux):
+        raise ValueError('Integrated flux is infinite')
+
+    #Get the standard unit spectrum in the renormalization units
+    RNunits=units.Units(RNunitstring)
+    if RNunits.isDensity:
+        up=RNunits.StdSpectrum*band
+    else:
+        up=RNunits.StdSpectrum
+
+    #Renormalize in magnitudes....
+    if RNunits.isMag:
+        ratio=totalflux/up.integrate()
+        dmag = RNval + 2.5*math.log10(ratio)
+        newsp = spectrum.addmag(dmag)
+        return newsp
+
+    #...or in linear flux units.
+    else:
+        const = RNval * (up.integrate()/totalflux)
+        newsp = spectrum*const
+        return newsp
