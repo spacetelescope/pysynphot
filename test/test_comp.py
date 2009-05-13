320920b8c7f8e7ce9fd3d955129cb2a0da1f65f9
laidler <laidler@stsci.edu>
2009-05-13 18:55:20 +0000

    Adding tests for refactored graph & comp tables
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@934 90a0a646-be8a-0410-bb88-9290da87bc01

---
+++ test/test_comp.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+try:
+    from pysynphot.comptab import Comptab
+except ImportError:
+    print "not implemented yet"
+
+class CompCase(TestCase):
+    def setUp(self):
+        self.fname='something'
+        self.C=Comptab(self.fname)
+
+    def test_nodups(self):
+        #Test for duplicate filenames.
+        #Duplicate compnames will be caught at construction
+        self.assert_(self.C._dupcheck())
+        
+    def testvalid(self):
+        #C is dictionary-like
+        self.assert_(self.C.validate())
+
+class BadComp(TestCase):
+    def setUp(self):
+        self.fname='somethingelse'
+
+    def testdupcomp(self):
+        self.assertRaises(Comptab,
+                          self.fname,
+                          KeyError)
+
+        
---
+++ test/test_graph.py
@@ -0,0 +1,165 @@
+from unittest import TestCase
+try:
+    from pysynphot.graphtab import GraphTab
+except ImportError:
+    print "Warning, the tests won't run; GraphTabl not yet implemented"
+
+def maketmg(strdata,tmgname):
+    """Helper function"""
+    import pyfits
+    out=open('data.txt','w')
+    out.write(strdata)
+    out.flush()
+    out.close()
+    tbhdu=pyfits.tcreate('data.txt',cdfile='cdfile.txt',hfile='h1.txt')
+    tbhdu.writeto(tmgname, clobber=True)
+      
+    
+class GraphCase(TestCase):
+    simple1="""
+acs 1 20 clear clear
+cos 1 20 clear clear
+default 1 100 clear clear
+noota 20 30 clear clear
+default 20 30 hst_ota clear
+ota 20 30 hst_ota clear
+acs 30 10000 clear clear
+cos 30 11000 clear clear
+sbc 10000 10199 clear clear
+hrc 10000 10150 clear clear
+wfc1 10000 10100 clear clear
+default 10100 10101 clear clear
+default 10101 10130 acs_wfc_im123 clear
+f606w 10130 10140 acs_f606w clear
+f550m 10130 10140 acs_f550m clear
+f555w 10130 10140 acs_f555w clear
+default 10140 10300 clear clear
+wfc1 10300 10310 acs_wfc_ebe_win12f clear
+default 10310 10320 acs_wfc_ccd1 clear
+mjd# 10310 10320 acs_wfc_ccd1_mjd clear
+"""
+    def setUp(self):
+        self.fname='/tmp/simple1.tmg'
+        make_tmg(simple1)
+        self.G=GraphTab(self.fname)
+
+    def tearDown(self):
+        os.unlink(self.fname)
+        os.unlink('data.txt')
+        
+    def test1(self):
+        self.instring='acs,wfc1,f555w'
+        self.ref=['hst_ota',
+                  'acs_wfc_im123',
+                  'acs_f555w',
+                  'acs_wfc_ebe_win12f',
+                  'acs_wfc_ccd1']
+        tst=self.G.traverse(self.instring)
+        self.assertEqual(self.ref,tst)
+
+    def testparam(self):
+        self.instring='acs,wfc1,f606w,mjd#70123'
+        self.ref=['hst_ota',
+                  'acs_wfc_im123',
+                  'acs_f606w',
+                  'acs_wfc_ebe_win12f',
+                  'acs_wfc_ccd1_mjd']
+        tst=self.G.traverse(self.instring)
+        self.assertEqual(self.ref,test)
+
+    def testnext(self):
+        self.instring="acs"
+        self.ref=set(['hrc','wfc1'])
+        tst=self.G.getnext(self.instring)
+
+    def testunused(self):
+        self.instring="cos,foo"
+        self.assertRaises(self.G.traverse,
+                          self.instring,
+                          ValueError)
+
+    def testincomplete(self):
+        self.instring="acs"
+        self.assertRaises(self.G.traverse,
+                          self.instring,
+                          ValueError)
+
+    def testambiguous(self):
+        self.instring="acs,wfc1,f550m,f555w"
+        self.assertRaises(self.G.traverse,
+                          self.instring,
+                          ValueError)
+
+    def testloopcheck(self):
+        self.assert_(self.G._loopcheck())
+
+    def testascending(self):
+        self.assert_(self.G._ordercheck())
+
+    def testreachable(self):
+        self.assert_(self.G._orphancheck())
+
+    def testvalidate(self):
+        #Performs all the earlier checks
+        self.assert_(self.G.validate())
+
+    def testallmodes(self):
+        self.ref=set(['cos',
+                      'acs, hrc',
+                      'acs, wfc1, f606w',
+                      'acs, wfc1, f550m',
+                      'acs, wfc1, f555w'])
+        tst=self.G.allmodes()
+        self.assertEqual(self.ref,tst)
+                        
+
+class ThermalCase(GraphCase):
+    simpletherm="""something with thermal modes"""
+
+    def setUp(self):
+        self.fname='simpletherm_tmg.fits'
+        self.G=GraphTable(self.fname)
+
+    def test_consistent(self):
+        self.assert_(self.G._thermback())
+
+    def test_opt_therm(self):
+        self.instring='nicmos,3,f222m'
+        #Is this the right UI? Or should traverse always return
+        #an (opt,thm) tuple?
+        opt=self.G.traverse(self.instring)
+        thm=self.G.traverse(self.instring,thermal=True)
+        #The thermal path must be a superset of the optical path,
+        #though it need not be a strict superset.
+        self.assert_(set(thm)>=set(opt))
+
+    def test_therm(self):
+        self.instring='nicmos,2,f187n'
+        self.ref='tbd'
+        tst=self.G.traverse(self.instring,thermal=True)
+        self.assertEqual(self.ref,tst)
+
+    def test_temp(self):
+        #Do we really want to keep this interface?
+        self.instring='nicmos,3,f222m,primary#320'
+        self.ref='tbd'
+        tst=self.G.traverse(self.instring,thermal=True)
+        #A better interface might be to specify temperature overrides
+        #when calling the method
+        #bp.thermback(primary=320)
+
+class BadGraph(TestCase):
+    badgraph="""something with loops and orphans and so on"""
+    def setUp(self):
+        self.fname='badgraph_tmg.fits'
+        self.G=GraphTab(self.fname)
+
+class MissingGraph(BadGraph):
+    missinggraph="""something with some missing columns"""
+    def setUp(self):
+        self.fname='missinggraph_tmg.fits'
+
+    def testconstructor(self):
+        self.assertRaises(GraphTab,
+                          self.fname,
+                          ValueError) #or some other error?)
