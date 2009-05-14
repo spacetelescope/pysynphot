b75bd0045115716f7cba5ca58845031bf3dd2f70
laidler <laidler@stsci.edu>
2009-05-14 17:46:47 +0000

    Adding tests for ticket150
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@936 90a0a646-be8a-0410-bb88-9290da87bc01

---
+++ test/ticket150.py
@@ -0,0 +1,55 @@
+import pysynphot as S
+from pysynphot.observation import Observation
+from pysynphot import etc
+import os, sys
+import testutil
+import numpy as N
+
+from pysynphot import locations, observationmode
+
+
+#Freeze the version of the comptable so tests are not susceptible to
+# updates to CDBS
+cmptb_name = os.path.join('mtab','t260548pm_tmc.fits')
+observationmode.COMPTABLE = observationmode._refTable(cmptb_name)
+print "%s:"%os.path.basename(__file__)
+print "  Tests are being run with %s"%observationmode.COMPTABLE
+
+#Also set the version of Vega for similar reasons
+locations.VegaFile=os.path.join('crcalspec',
+                                'alpha_lyr_stis_003.fits')
+print "Using Vega spectrum: %s"%locations.VegaFile
+
+class RenormOverlap(testutil.FPTestCase):
+    """Tests for strict rejection."""
+    def setUp(self):
+        #(re)discovery case: stis_rn_cases/stisC94
+        self.sp=S.FileSpectrum('$PYSYN_CDBS/grid/kc96/starb2_template.fits')
+        self.bp=S.ObsBandpass('cos,fuv,g130m,c1300')
+        self.cmd='rn(crgridkc96$starb2_template.fits,band(cos,fuv,g130m,c1300),16.0,stmag)'
+        self.ref=0.00718543 #expected renorm factor
+        
+    def testraise(self):
+        self.assertRaises(ValueError,
+                          self.sp.renorm,
+                          16.0,'stmag',self.bp)
+
+    def testforce(self):
+        sp2=self.sp.renorm(16.0,'stmag',self.bp,force=True)
+        ratio=sp2.flux/self.sp.flux
+        self.failUnless(N.all(1-abs(ratio/self.ref)<0.0001))
+
+    def testparse(self):
+        sp2=S.etc.parse_spec(self.cmd)
+        ratio=sp2.flux/self.sp.flux
+        self.failUnless(N.all(1-abs(ratio/self.ref)<0.0001))
+
+    def testfull(self):
+        jv=S.ObsBandpass('johnson,v')
+        try:
+            sp2=sp.renorm(17.0,'abmag',jv)
+        except ValueError,e:
+            self.fail(e.msg)
+      
+
+                       
