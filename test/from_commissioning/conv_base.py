43dd2f7f8dfea215131282229fc3fc632de5a866
laidler <laidler@stsci.edu>
2010-03-05 19:53:57 +0000

    Force extrapolation for partial overlap cases. This is consistent with what was done in the syn_pysyn commissioning test set.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@1230 90a0a646-be8a-0410-bb88-9290da87bc01

--- test/from_commissioning/conv_base.py
+++ test/from_commissioning/conv_base.py
@@ -91,9 +91,15 @@ class SpecCase(object):
             try:
                 cls.obs = S.Observation(cls.sp, cls.bp)
             except ValueError, e:
-                cls.tra['obs_error']=str(e)
-                cls.obs = str(e)
-                return #then the obs tests should raise errors
+                #Force extrapolation in the case of partialoverlap
+                try:
+                    cls.obs = S.Observation(cls.sp, cls.bp, force='extrap')
+                    cls.tda['extrap']=True
+                except ValueError, e:
+                    cls.tra['obs_error']=str(e)
+                    cls.obs = str(e)
+                    return #then the obs tests should raise errors
+                
             cls.obs.convert('counts')
             x = dict(PSCNTRAT = (cls.obs.countrate(),'countrate'),
                      PSEFFLAM = (cls.obs.efflam(),'efflam'))
