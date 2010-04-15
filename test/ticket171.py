fe46dabf079e2186b22033e7d4b1f56d69e5cdac
laidler <laidler@stsci.edu>
2010-04-15 18:02:19 +0000

    oops, never committed this test
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@1289 90a0a646-be8a-0410-bb88-9290da87bc01

---
+++ test/ticket171.py
@@ -0,0 +1,21 @@
+import os
+import re
+import unittest
+
+#Code under test
+from pysynphot.locations import irafconvert
+
+class VarString(unittest.TestCase):
+    def setUp(self):
+        os.environ['PYSYN_CDBS'] = os.path.abspath(os.path.dirname(__file__))
+        self.fstring = '$PYSYN_CDBS/etc/background/Zodi.fits'
+        self.ref = os.path.join(os.environ['PYSYN_CDBS'],
+                                'etc/background/Zodi.fits')
+        self.tda=dict(pysyn_cdbs = os.path.dirname(__file__),
+                      sep = os.path.sep,
+                      fstring=self.fstring,
+                      ref=self.ref)
+    def test1(self):
+        ans = irafconvert(self.fstring)
+        self.tra=dict(ans=ans)
+        self.assertEqual(self.ref, ans)
