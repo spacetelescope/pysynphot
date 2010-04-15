45107c2c939bda20df573a736d0c31f273dd3268
laidler <laidler@stsci.edu>
2010-04-15 16:45:04 +0000

    added more tests
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@1284 90a0a646-be8a-0410-bb88-9290da87bc01

--- test/v06_tickets.py
+++ test/v06_tickets.py
@@ -1,23 +1,25 @@
 from __future__ import division
 import sys
 import os
+import unittest
 import testutil
 import pysynphot as S
 import numpy as N
 
-class Ticket128(testutil.FPTestCase):
-    def setUp(self):
-        self.sp=S.FlatSpectrum(10)
-        self.bp=S.Box(10000,100)
+class AnalyticBP(testutil.FPTestCase):
 
-    def testsp(self):
-        tst=self.sp(5000)
-        self.assert_(tst == 10)
+    def setUp(self):
+        self.bp=S.Box(10000,100)
 
     def testbp1(self):
         tst=self.bp(10000)
         self.assert_(tst == 1.0)
 
+    def testconstant(self):
+        bp2 = self.bp * 3
+        tst = bp2(10000)
+        self.assert_(tst == 3)
+        
     def testbp2(self):
         tst=self.bp(3000)
         self.assert_(tst == 0.0)
@@ -29,3 +31,66 @@ class Ticket128(testutil.FPTestCase):
     def testbp4(self):
         tst=self.bp.sample(3000)
         self.assert_(tst == 0.0)
+
+    def testflat(self):
+        self.bp = S.UniformTransmission(0.5)
+        tst = self.bp.sample(3000)
+        self.assert_(tst == 0.5)
+
+
+        
+
+class AnalyticSP(testutil.FPTestCase):
+    def setUp(self):
+        self.sp=S.FlatSpectrum(10)
+
+    def testsp(self):
+        tst=self.sp(5000)
+        self.assert_(tst == 10)
+
+    def testcompspec(self):
+        self.sp=S.BlackBody(5500)+S.FlatSpectrum(1)
+        tst=self.sp(3000)
+        assert True
+
+class Tabular(testutil.FPTestCase):
+    def setUp(self):
+        #Make arrays
+        self.wv = N.arange(1000,2000)
+        self.fl = N.zeros(self.wv.shape)
+        self.fl[100:-100] = 10.0
+        
+    def testsp(self):
+        self.sp=S.ArraySpectrum(self.wv,self.fl,
+                                fluxunits='photlam')
+        tst=self.sp(1500)
+        self.assert_(tst == 10)
+
+    def testcompsp(self):
+        sp1=S.ArraySpectrum(self.wv,self.fl, fluxunits='photlam')
+        sp2=S.ArraySpectrum(self.wv[48:],self.fl[48:]*2.3,
+                            fluxunits='photlam')
+        self.sp=sp1 + sp2
+        tst=self.sp(1500)
+        self.assert_(tst == 10+(10*2.3))
+
+        
+    def testbp(self):
+        self.bp=S.ArrayBandpass(self.wv,self.fl)
+        tst=self.bp(1500)
+        self.assert_(tst == 10)
+
+class TestDoesntError(unittest.TestCase):
+    #Just test to make sure it doesn't raise an exception;
+    #don't test that the actual value is correct
+
+    def testobsband(self):
+        self.bp=S.ObsBandpass('acs,hrc,f555w')
+        tst=self.bp(3000)
+        assert True
+        
+
+    def testicat(self):
+        self.sp=S.Icat('k93models',4500,1,2)
+        tst=self.sp(3000)
+        assert True
