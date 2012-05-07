f1e2a0a0409b09089b7e7c4d8573bb7307216227
stsci_sienkiew <stsci_sienkiew@stsci.edu>
2012-05-07 14:29:51 +0000

    hack for pysynphot to get svn_version.py in pyetc
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@2731 90a0a646-be8a-0410-bb88-9290da87bc01

--- defsetup.py
+++ defsetup.py
@@ -56,3 +56,21 @@ setupargs = {
 
 }
 
+# We want to do this specially for pyetc.  In pyetc, the support libraries
+# are a different version than in the latest stsci_python, so svn_version.py
+# is created in the wrong place.  This code makes the old library use the
+# new location, so that the svn info is actually installed.
+try :
+    import pyetc
+except ImportError, e :
+    # not in a pyetc system
+    pass
+else :
+    # The svn_version file defaults to lib/svn_version.py but we are
+    # installing it from lib/pysynphot/svn_version.py - so, here in defsetup
+    # we create the file in the correct place.  The new library will want
+    # to create it again when we install.  The old library will create it
+    # again in the wrong place, but we don't care.
+    import stsci.tools.stsci_distutils_hack
+    stsci.tools.stsci_distutils_hack.__set_svn_version__( fname="pysynphot/svn_version.py" )
+
