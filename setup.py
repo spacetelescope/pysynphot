37226cc12727df372f0fffd5a1b44848e7173c09
stsci_embray <stsci_embray@stsci.edu>
2012-08-01 19:29:11 +0000

    Update the stsci.distutils version
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/pysynphot/trunk@2897 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     from setuptools import setup
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3dev'],
+    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3'],
     dependency_links=['http://stsdas.stsci.edu/download/packages'],
     d2to1=True,
     use_2to3=True,
