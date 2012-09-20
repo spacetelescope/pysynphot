b55246bec777ddcce3cb935796812ec213b5c5fe
stsci_laidler <stsci_laidler@stsci.edu>
2012-09-20 21:06:40 +0000

    #237: Make the parser accept a # as a valid part of an argument
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@2984 90a0a646-be8a-0410-bb88-9290da87bc01

--- test/test_parsing.py
+++ test/test_parsing.py
@@ -3,3 +3,8 @@ import pysynphot.spparser as parser
 def test_double_slash():
   sp = parser.parse_spec("spec($PYSYN_CDBS//calspec/gd71_mod_005.fits)")
   assert True
+
+def test_pound():
+  sp = parser.parse_spec("rn(unit(1.,flam),band(acs,wfc1,fr388n#3881.0),10.000000,abmag)")
+  assert True
+  
