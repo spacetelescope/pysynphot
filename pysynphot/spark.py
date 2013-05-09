spacetelescope/pysynphot

commit a42040d4e9a1d921edac331ac42d9b0f74531200

Fix for #1007
git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@3405 90a0a646-be8a-0410-bb88-9290da87bc01

pysynphot/spark.py
@@ -32,7 +32,7 @@ def _namelist(instance):
                 for b in c.__bases__:
                         classlist.append(b)
                 for name in dir(c):
-                        if not namedict.has_key(name):
+                        if name not in namedict:
                                 namelist.append(name)
                                 namedict[name] = 1
         return namelist
@@ -74,7 +74,7 @@ class GenericScanner:
 
                         groups = m.groups()
                         for i in range(len(groups)):
-                                if groups[i] and self.index2func.has_key(i):
+                                if groups[i] and i in self.index2func:
                                         self.index2func[i](groups[i])
                         pos = m.end()
 
@@ -115,7 +115,7 @@ class GenericParser:
 
                         rule, fn = self.preprocess(rule, func)
 
-                        if self.rules.has_key(lhs):
+                        if lhs in self.rules:
                                 self.rules[lhs].append(rule)
                         else:
                                 self.rules[lhs] = [ rule ]
@@ -148,7 +148,7 @@ class GenericParser:
                 
                 for rulelist in self.rules.values():
                         for lhs, rhs in rulelist:
-                                if not self.first.has_key(lhs):
+                                if lhs not in self.first:
                                         self.first[lhs] = {}
 
                                 if len(rhs) == 0:
@@ -156,7 +156,7 @@ class GenericParser:
                                         continue
 
                                 sym = rhs[0]
-                                if not self.rules.has_key(sym):
+                                if sym not in self.rules:
                                         self.first[lhs][sym] = 1
                                 else:
                                         union[(sym, lhs)] = 1
@@ -246,7 +246,7 @@ class GenericParser:
                         #
                         #  A -> a . B (predictor)
                         #
-                        if self.rules.has_key(nextSym):
+                        if nextSym in self.rules:
                                 #
                                 #  Work on completer step some more; for rules
                                 #  with empty RHS, the "parent state" is the
@@ -254,7 +254,7 @@ class GenericParser:
                                 #  so the Earley items the completer step needs
                                 #  may not all be present when it runs.
                                 #
-                                if needsCompletion.has_key(nextSym):
+                                if nextSym in needsCompletion:
                                         new = (rule, pos+1, parent)
                                         olditem_i = needsCompletion[nextSym]
                                         if new not in state:
@@ -266,7 +266,7 @@ class GenericParser:
                                 #
                                 #  Has this been predicted already?
                                 #
-                                if predicted.has_key(nextSym):
+                                if nextSym in predicted:
                                         continue
                                 predicted[nextSym] = 1
 
@@ -290,15 +290,15 @@ class GenericParser:
                                                         state.append(new)
                                                         continue
                                                 prhs0 = prhs[0]
-                                                if not self.rules.has_key(prhs0):
+                                                if prhs0 not in self.rules:
                                                         if prhs0 != ttype:
                                                                 continue
                                                         else:
                                                                 state.append(new)
                                                                 continue
                                                 first = self.first[prhs0]
-                                                if not first.has_key(None) and \
-                                                   not first.has_key(ttype):
+                                                if None not in first and \
+                                                   ttype not in not first:
                                                         continue
                                                 state.append(new)
                                         continue
@@ -311,7 +311,7 @@ class GenericParser:
                                         #
                                         prhs = prule[1]
                                         if len(prhs) > 0 and \
-                                           not self.rules.has_key(prhs[0]) and \
+                                           prhs[0] not in self.rules and \
                                            token != prhs[0]:
                                                 continue
                                         state.append((prule, 0, i))
@@ -333,7 +333,7 @@ class GenericParser:
                 
                 while pos > 0:
                         want = ((rule, pos, parent), state)
-                        if not tree.has_key(want):
+                        if want not in tree:
                                 #
                                 #  Since pos > 0, it didn't come from closure,
                                 #  and if it isn't in tree[], then there must

commit 2e33129cf65ec6edb696db02ceb5675d8e574313

fix a typo introduced in r3405
git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@3406 90a0a646-be8a-0410-bb88-9290da87bc01

pysynphot/spark.py
@@ -298,7 +298,7 @@ class GenericParser:
                                                                 continue
                                                 first = self.first[prhs0]
                                                 if None not in first and \
-                                                   ttype not in not first:
+                                                   ttype not in first:
                                                         continue
                                                 state.append(new)
                                         continue