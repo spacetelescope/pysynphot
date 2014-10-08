d82afc848c2aa86799b2e9b4c62e8150aeb2efa5
cslocum <cslocum@stsci.edu>
2014-10-08 21:11:19 +0000

    fix a few version related things in pysynphot trunk
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pysynphot@4313 90a0a646-be8a-0410-bb88-9290da87bc01

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ except ImportError:
 
 setup(
     setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3'],
-    dependency_links=['http://stsdas.stsci.edu/download/packages'],
     d2to1=True,
     use_2to3=True,
     zip_safe=False
