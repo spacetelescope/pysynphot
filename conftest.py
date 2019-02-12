a8ead8a91f29d94e43273f72e74ed47ac98a785e
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-02-12 13:57:41 -0500

    Hulk smash

--- conftest.py
+++ conftest.py
@@ -1,13 +1,8 @@
 import pytest  # noqa
 
-from astropy.tests.helper import (enable_deprecations_as_exceptions,
-                                  treat_deprecations_as_exceptions)
+from astropy.tests.helper import enable_deprecations_as_exceptions
 
 enable_deprecations_as_exceptions()
-
-
-def pytest_configure(config):
-    treat_deprecations_as_exceptions()
 
 
 def pytest_addoption(parser):
