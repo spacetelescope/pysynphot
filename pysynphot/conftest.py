23a7586e767e7e0fce46860f3fa3e0db7cc7968c
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-06-02 14:11:11 -0400

    Remove usage of deprecated test helpers

--- pysynphot/conftest.py
+++ pysynphot/conftest.py
@@ -1,15 +1,8 @@
 import os
-
-from astropy.tests.helper import (enable_deprecations_as_exceptions,
-                                  treat_deprecations_as_exceptions)
 from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
-
-enable_deprecations_as_exceptions()
 
 
 def pytest_configure(config):
-    treat_deprecations_as_exceptions()
-
     # Configure header here.
     PYTEST_HEADER_MODULES['astropy'] = 'astropy'
     PYTEST_HEADER_MODULES.pop('h5py', None)
