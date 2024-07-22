b9c684251eee361e55e91fb6eebf7a554a3ae465
isaacspitzer <ispitzer@stsci.edu>
2024-07-22 08:44:07 -0400

    Fixing import

--- pysynphot/test/test_observation.py
+++ pysynphot/test/test_observation.py
@@ -7,7 +7,7 @@ import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_array_equal
 
-from Pandeia.src.pysynphot import pysynphot
+import pysynphot
 
 from ..observation import Observation
 from ..obsbandpass import ObsBandpass
